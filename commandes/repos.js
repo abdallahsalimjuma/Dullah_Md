bf4ceeed8d1935a25fa4b0e8733d29115137bed9
FredieTech <frediezra60@gmail.com>
2024-08-26 19:45:58 +0300

    Update repos.js

--- commandes/repos.js
+++ commandes/repos.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 
 zokou({ nomCom: "repo", catégorie:"Général", reaction: "✨", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-  const githubRepo = 'https://api.github.com/repos/Fred1e/Dullah-Md';
-  const img = 'https://telegra.ph/file/dcaaa16f7b556d84093f7.jpg';
+  const githubRepo = 'https://api.github.com/repos/abdallahsalimjuma/Dullah_Md';
+  const img = 'https://telegra.ph/file/bb24465612098159bb5a9.jpg';
 
   try {
     const response = await fetch(githubRepo);
