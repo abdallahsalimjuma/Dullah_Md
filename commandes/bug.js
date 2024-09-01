fa45a731f4cb07f6fec26269eed9aecbe0a14b74
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2024-09-01 12:13:40 +0200

    Update bug.js

--- commandes/bug.js
+++ commandes/bug.js
@@ -34,7 +34,7 @@ const temps = moment().format('HH:mm:ss');
 const date = moment().format('DD/MM/YYYY');
 
 let infoMsg =  `
-━━━━❍ *DULLAH MD CRASHER* ❍━━━━
+━━━━❍ *DULLAH MD BOT CRASHER* ❍━━━━
 ╭─────────────────────❍ 
 │ bug
 │ crash
