e61d958367146cc818d25f9ee2513b4cb41b0710
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2024-10-01 15:40:06 +0200

    Update quote.js

--- commandes/quote.js
+++ commandes/quote.js
@@ -13,14 +13,14 @@ zokou({ nomCom: 'quote', categorie: 'Fun' }, async (dest, zk, commandeOptions) =
         .then((response) => response.json())
         .then(async (quote) => {
           repondre(`╔══════════════════════════╗
-║   Boniphace-md               ║
+║   Dullah-md               ║
 ╚══════════════════════════╝
 
 🎬 Anime: ${quote.anime}
 👤 Character: ${quote.character}
 💬 Quote: ${quote.quote}
 
-Powered by Boniphace-MD`);
+Powered by Dullah-MD`);
         });
     } catch (e) {
       repondre('Erreur lors de la génération de la citation : ' + e.message);
@@ -33,14 +33,14 @@ Powered by Boniphace-MD`);
         .then((response) => response.json())
         .then(async (quote) => {
           repondre(`╔══════════════════════════╗
-║   Boniphace-md               ║
+║   Dullah-md               ║
 ╚══════════════════════════╝
 
 🎬 Anime: ${quote.anime}
 👤 Character: ${quote.character}
 💬 Quote: ${quote.quote}
 
-Powered by Hacking-MD`);
+Powered by Dullah-Md bot`);
         });
     } catch (e) {
       repondre('Erreur lors de la génération de la citation : ' + e.message);
